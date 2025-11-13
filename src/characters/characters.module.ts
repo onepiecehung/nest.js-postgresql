b7a4be3852abbde55f990b1ce0d5798f0545becf
onepiecehung <360anime.suki@gmail.com>
2025-11-13 14:08:02 +0700

    Refactor Characters Module to Simplify Imports
    - Updated the `CharactersModule` to streamline the import statements by removing the unnecessary `SeriesCharacter` entity from the `TypeOrmModule.forFeature` method, enhancing clarity and maintainability.
    - This change aligns with the project's commitment to manual updates and adherence to established patterns, prohibiting auto-generations and migrations.

--- src/characters/characters.module.ts
+++ src/characters/characters.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReactionsModule } from 'src/reactions/reactions.module';
 import { CharactersController } from './characters.controller';
 import { CharactersService } from './characters.service';
 import { Character } from './entities/character.entity';
-import { SeriesCharacter } from 'src/series/entities/series-character.entity';
-import { ReactionsModule } from 'src/reactions/reactions.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Character, SeriesCharacter]),
-    ReactionsModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Character]), ReactionsModule],
   controllers: [CharactersController],
   providers: [CharactersService],
   exports: [CharactersService],
